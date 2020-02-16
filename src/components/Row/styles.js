166fc96c35ba85908e393dd91fb5910f0ec8c10d
Vitor Leonel <vitor.leonel@picpay.com>
2020-02-16 17:57:33 -0300

    add margin and padding on row

--- src/components/Row/styles.js
+++ src/components/Row/styles.js
@@ -4,4 +4,19 @@ export const Container = styled.View`
   flex-direction: ${props => props.direction || "column"};
   justify-content: ${props => props.justifyContent || "flex-start"};
   align-items: ${props => props.alignItems || "stretch"};
+
+  margin-top: ${props => (props.marginTop ? `${props.marginTop}px` : "0")};
+  margin-bottom: ${props =>
+    props.marginBottom ? `${props.marginBottom}px` : "0"};
+  margin-left: ${props => (props.marginLeft ? `${props.marginLeft}px` : "0")};
+  margin-right: ${props =>
+    props.marginRight ? `${props.marginRight}px` : "0"};
+
+  padding-top: ${props => (props.paddingTop ? `${props.paddingTop}px` : "0")};
+  padding-bottom: ${props =>
+    props.paddingBottom ? `${props.paddingBottom}px` : "0"};
+  padding-left: ${props =>
+    props.paddingLeft ? `${props.paddingLeft}px` : "0"};
+  padding-right: ${props =>
+    props.paddingRight ? `${props.paddingRight}px` : "0"};
 `;
