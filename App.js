62c19503d3725c8f9c0c2f541b4f4c197f0bd065
Vitor Leonel <vitor.leonel@picpay.com>
2020-02-16 18:30:01 -0300

    set barstyle

--- App.js
+++ App.js
@@ -1,4 +1,8 @@
 import React from "react";
+import { StatusBar } from "react-native";
+
+StatusBar.setBarStyle("dark-content");
+
 import Routes from "./src/routes";
 
 export default function App() {
