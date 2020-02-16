54e80f554f91823ab045804a12e8dbf342826188
Vitor Leonel <vitor.leonel@picpay.com>
2020-02-16 16:01:45 -0300

    adjust button for receive icon

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -7,7 +7,18 @@ import Text from "../Text";
 export default function Button(props) {
   return (
     <Container {...props}>
-      <Text color={props.textColor || "#212330"}>{props.text}</Text>
+      {props.leftIcon}
+
+      <Text
+        color={props.textColor || "#212330"}
+        weight={props.textWeight}
+        marginLeft={props.leftIcon && "8"}
+        marginRight={props.rightIcon && "8"}
+      >
+        {props.text}
+      </Text>
+
+      {props.rightIcon}
     </Container>
   );
 }
--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -14,6 +14,7 @@ export const Container = styled.TouchableOpacity.attrs({
     props.marginRight ? `${props.marginRight}px` : "0"};
 
   border-radius: 10px;
+  flex-direction: row;
   justify-content: center;
   align-items: center;
 `;
