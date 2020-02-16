5f69be74fb9666361f62172ce52fb21b77f8115d
Vitor Leonel <vitor.leonel@picpay.com>
2020-02-16 16:01:57 -0300

    wrapper for react-vector-icons

---
+++ src/components/Icon/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+export default function Icon(props) {
+  return <Container {...props} />;
+}
---
+++ src/components/Icon/styles.js
@@ -0,0 +1,4 @@
+import styled from "styled-components/native";
+import Icons from "@expo/vector-icons/FontAwesome";
+
+export const Container = styled(Icons)``;
